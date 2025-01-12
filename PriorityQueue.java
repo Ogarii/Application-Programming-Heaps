0fdccfcbb3156489f2c9a6b3fe06f13d2d208626
Ogarii <mbonuke33@gmail.com>
2025-01-12 14:19:57 +0300

    prority queue interface

---
+++ PriorityQueue.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public interface PriorityQueue {
+    void insert(int value); // Method to insert a value into the priority queue
+    int extract(); // Method to extract the highest or lowest priority value
+    boolean isEmpty(); // Method to check if the priority queue is empty
+
+}
